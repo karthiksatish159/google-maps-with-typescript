7c941cb424dd590a6ad1e4e43f6b5543a24ed570
Karthiksatish <karthiksatish159@gmail.com>
2023-05-04 16:47:03 +0530

    google maps is integerated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { User } from "./User";
-import { Company } from "./Company";
-const user=new User();
-console.log(user);
-const company=new Company();
-google
+// import { User } from "./User";
+// import { Company } from "./Company";
+// const user=new User();
+// console.log(user);
+// const company=new Company();
+new google.maps.Map(document.getElementById("map") as HTMLElement,
+{
+    zoom:8,
+    center:
+    {
+        lat:0,
+        lng:0
+    }
+});
