8b0ef7ba2143d9bd99058891125331f87a311d21
Karthiksatish <karthiksatish159@gmail.com>
2023-05-04 18:03:04 +0530

    here i was centering the my current location

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@
 // const user=new User();
 // console.log(user);
 // const company=new Company();
-new google.maps.Map(document.getElementById("map") as HTMLElement,
+(()=>
 {
-    zoom:8,
-    center:
+    navigator.geolocation.getCurrentPosition(function(data)
     {
-        lat:0,
-        lng:0
-    }
-});
+        new google.maps.Map(document.getElementById("map") as HTMLElement,
+        {
+            zoom:20,
+            center:
+            {
+                lat:data.coords.latitude,
+                lng:data.coords.longitude
+            }
+        });
+    })
+})()
