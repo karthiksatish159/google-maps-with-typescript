41c5a318da9caead5429f0fd8fc43b9183dc8948
Karthik Satish Telagareddy <karthik.st@holosuit.com>
2023-05-04 10:49:01 +0530

    intial implementation of user

--- src/User.ts
+++ src/User.ts
@@ -7,4 +7,13 @@ class User
         lat:number,
         lng:number
     }
+    constructor()
+    {
+        this.name=faker.name.firstName();
+        this.location=
+        {
+            lat:parseFloat(faker.address.latitude()),
+            lng:parseFloat(faker.address.longitude())
+        }
+    }
 }
